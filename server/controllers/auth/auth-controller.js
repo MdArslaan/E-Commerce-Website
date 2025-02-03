b563f0a63710db5de3a4a76e285a8b51565c16c7
MdArslaan <142446633+MdArslaan@users.noreply.github.com>
2025-02-03 11:26:10 +0530

    Tailwind Changes

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -65,7 +65,7 @@ const loginUser = async (req, res) => {
         userName: checkUser.userName,
       },
       "CLIENT_SECRET_KEY",
-      { expiresIn: "60m/s" }
+      { expiresIn: "60m" }
     );
 
     res.cookie("token", token, { httpOnly: true, secure: false }).json({
